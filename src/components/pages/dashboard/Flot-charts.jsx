f3d65a8532a5b3109c074e9f358b7c139d9f1f93
Sanket <sanket@sahusoft.com>
2015-10-22 17:54:32 +0530

    Fixes to url

--- src/components/pages/dashboard/Flot-charts.jsx
+++ src/components/pages/dashboard/Flot-charts.jsx
@@ -59,7 +59,7 @@ var FlotCharts = React.createClass({
             <div className="col-lg-12">
                 <Panel header=<span>Flot Charts Usage</span> >
                     <div>
-                        <p>Flot is a pure JavaScript plotting library for jQuery, with a focus on simple usage, attractive looks, and interactive features. In SB Admin, we are using the most recent version of Flot along with a few plugins to enhance the user experience. The Flot plugins being used are the tooltip plugin for hoverable tooltips, and the resize plugin for fully responsive charts. The documentation for Flot Charts is available on their website, <a target="_blank" href="http://www.flotcharts.org/">"http://www.flotcharts.org/"</a>.</p>
+                        <p>Flot is a pure JavaScript plotting library for jQuery, with a focus on simple usage, attractive looks, and interactive features. In SB Admin, we are using the most recent version of Flot along with a few plugins to enhance the user experience. The Flot plugins being used are the tooltip plugin for hoverable tooltips, and the resize plugin for fully responsive charts. The documentation for Flot Charts is available on their website, <a target="_blank" href="http://www.flotcharts.org/">http://www.flotcharts.org/</a>.</p>
                         <Button bsSize="large" block href="http://www.flotcharts.org/">View Flot Charts Documentation</Button>
                     </div>
                 </Panel>
