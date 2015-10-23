f12f9e4176c4f5732d9af01dc9fe5437793315e2
sarika gautam <sarika@sahusoft.com>
2015-10-23 15:33:21 +0530

    changes to routes file

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -19,31 +19,38 @@ import DashboardMorrisjsChartsPage from "../components/pages/dashboard/MorrisjsC
 import LoginPage from "../components/pages/Login";
 import LogoutPage from "../components/pages/Logout";
 
-export default class Routes {
+var Routes = React.createClass({
 
-  static getRoutes = function() {
-    return (
-        <Route name="base" path="/" handler={BaseLayout}>
-          <Route name="dashboard" path="/dashboard" handler={DashboardLayout}>
-            <Route name="dashboard.home" path="/home" handler={DashboardHomePage} />
-            <Route name="dashboard.flot-charts" path="/flot-charts" handler={DashboardFlotChartsPage} />
-            <Route name="dashboard.morrisjs-charts" path="/morrisjs-charts" handler={DashboardMorrisjsChartsPage} />
-            <Route name="dashboard.tables" path="/tables" handler={DashboardTablesPage} />
-            <Route name="dashboard.forms" path="/forms" handler={DashboardFormsPage} />
-            <Route name="dashboard.panels-wells" path="/panels-wells" handler={DashboardPanelsWellsPage} />
-            <Route name="dashboard.buttons" path="/buttons" handler={DashboardButtonsPage} />
-            <Route name="dashboard.notifications" path="/notifications" handler={DashboardNotificationsPage} />
-            <Route name="dashboard.typography" path="/typography" handler={DashboardTypographyPage} />
-            <Route name="dashboard.icons" path="/icons" handler={DashboardIconsPage} />
-            <Route name="dashboard.grid" path="/grid" handler={DashboardGridPage} />
-            <Route name="dashboard.blank" path="/blank" handler={DashboardBlankPage} />
-            <DefaultRoute name="dashboard.default" handler={DashboardHomePage} />
+  statics: {
+    getRoutes: function() {
+      return (
+          <Route name="base" path="/" handler={BaseLayout}>
+            <Route name="dashboard" path="/dashboard" handler={DashboardLayout}>
+              <Route name="dashboard.home" path="/home" handler={DashboardHomePage} />
+              <Route name="dashboard.flot-charts" path="/flot-charts" handler={DashboardFlotChartsPage} />
+              <Route name="dashboard.morrisjs-charts" path="/morrisjs-charts" handler={DashboardMorrisjsChartsPage} />
+              <Route name="dashboard.tables" path="/tables" handler={DashboardTablesPage} />
+              <Route name="dashboard.forms" path="/forms" handler={DashboardFormsPage} />
+              <Route name="dashboard.panels-wells" path="/panels-wells" handler={DashboardPanelsWellsPage} />
+              <Route name="dashboard.buttons" path="/buttons" handler={DashboardButtonsPage} />
+              <Route name="dashboard.notifications" path="/notifications" handler={DashboardNotificationsPage} />
+              <Route name="dashboard.typography" path="/typography" handler={DashboardTypographyPage} />
+              <Route name="dashboard.icons" path="/icons" handler={DashboardIconsPage} />
+              <Route name="dashboard.grid" path="/grid" handler={DashboardGridPage} />
+              <Route name="dashboard.blank" path="/blank" handler={DashboardBlankPage} />
+              <DefaultRoute name="dashboard.default" handler={DashboardHomePage} />
+            </Route>
+            <Route name="login" path="/login" handler={LoginPage} />
+            <Route name="logout" path="/logout" handler={LogoutPage} />
+            <DefaultRoute name="index" handler={LoginPage} />
           </Route>
-          <Route name="login" path="/login" handler={LoginPage} />
-          <Route name="logout" path="/logout" handler={LogoutPage} />
-          <DefaultRoute name="index" handler={LoginPage} />
-        </Route>
-    );
+      );
+    }
+  },
+  render: function() {
+  
   }
+  
+});
 
-}
+export default Routes;
