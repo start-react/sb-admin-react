5e2ba40ad2b07c37adf6629323129bd55ca70ada
sarika gautam <sarika@sahusoft.com>
2016-03-01 11:51:27 +0530

    fix for hamburger and menu on small screen

--- src/components/layouts/Dashboard/index.jsx
+++ src/components/layouts/Dashboard/index.jsx
@@ -33,6 +33,15 @@ var HomePage = React.createClass({
 
   },
 
+  toggleMenu: function(){
+    if($(".navbar-collapse").hasClass('collapse')){
+      $(".navbar-collapse").removeClass('collapse');  
+    }
+    else{
+      $(".navbar-collapse").addClass('collapse');
+    }
+  },
+
   render: function() {
 
     return (
@@ -40,122 +49,129 @@ var HomePage = React.createClass({
 
           <Navbar brand={<span><img src={require('../../../common/img/logo.png')} alt="Start React" title="Start React" />
             <span>&nbsp;SB Admin React - </span>
-            <a href="http://startreact.com/" title="Start React" rel="home">StartReact.com</a></span>} fluid={true}  style={ {margin: 0} }>
-          
-            <Nav style={ {margin: 0} } pullRight >
-          
-              <NavDropdown title=<i className="fa fa-envelope fa-fw"></i> pullRight >
-                <MenuItem eventKey="1">
-                  <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-
-                </MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey="2">
-                  <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                </MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey="3">
-                  <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div> 
-                </MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey="4">
-                  <strong>Read All Messages</strong> <i className="fa fa-angle-right"></i>
-                </MenuItem>
-              </NavDropdown>
-
-              <NavDropdown title=<i className="fa fa-tasks fa-fw"></i> pullRight >
-                <MenuItem eventKey="1" style={ {width: 300} }>
-                  <div> 
-                    <p> <strong>Task 1</strong> <span className="pull-right text-muted">40% Complete</span> </p> 
-                    <div>
-                      <ProgressBar active bsStyle="success" now={40} /> 
-                    </div> 
-                  </div>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="2">
-                  <div> 
-                    <p> <strong>Task 2</strong> <span className="pull-right text-muted">20% Complete</span> </p> 
-                    <div>
-                      <ProgressBar active bsStyle="info" now={20} />
-                    </div> 
-                  </div> 
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="3">
-                  <div> 
-                    <p> <strong>Task 3</strong> <span className="pull-right text-muted">60% Complete</span> </p> 
-                    <div>
-                      <ProgressBar active bsStyle="warning" now={60} />
-                    </div> 
-                  </div> 
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="4">
-                  <div> 
-                    <p> <strong>Task 4</strong> <span className="pull-right text-muted">80% Complete</span> </p> 
-                    <div>
-                      <ProgressBar active bsStyle="danger" now={80} />
-                    </div> 
-                  </div>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="5">
-                    <strong>See All Tasks</strong> <i className="fa fa-angle-right"></i>
-                </MenuItem>
-              </NavDropdown>
-
-              <NavDropdown title=<i className="fa fa-bell fa-fw"></i> pullRight >
-                <MenuItem eventKey="1" style={ {width: 300} }>
-                  <div> <i className="fa fa-comment fa-fw"></i> New Comment <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="2">
-                  <div> <i className="fa fa-twitter fa-fw"></i> 3 New Followers <span className="pull-right text-muted small">12 minutes ago</span> </div>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="3">
-                  <div> <i className="fa fa-envelope fa-fw"></i> Message Sent <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="4">
-                  <div> <i className="fa fa-tasks fa-fw"></i> New Task <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="5">
-                  <div> <i className="fa fa-upload fa-fw"></i> Server Rebooted <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="6">
-                  <strong>See All Alerts</strong> <i className="fa fa-angle-right"></i>
-                </MenuItem>
-              </NavDropdown>
-
-              <NavDropdown title=<i className="fa fa-user fa-fw"></i> pullRight >
-                <MenuItem eventKey="1">
-                  <i className="fa fa-user fa-fw"></i> User Profile
-                </MenuItem> 
-                <MenuItem eventKey="2">
-                  <i className="fa fa-gear fa-fw"></i> Settings
-                </MenuItem> 
-                <MenuItem eventKey="3">
-                  <a href="http://www.strapui.com/" onClick={ () => { window.location='http://www.strapui.com/'; } }>
-                    <i className="fa fa-eye fa-fw"></i> Premium React Themes
-                  </a>
-                </MenuItem> 
-                <MenuItem divider />
-                <MenuItem eventKey="4">
-                  <Link to="login">
-                    <i className="fa fa-sign-out fa-fw"></i> Logout
-                  </Link>
-                </MenuItem>
-              </NavDropdown>
-
-            </Nav>
+            <a href="http://startreact.com/" title="Start React" rel="home">StartReact.com</a>
+            <button type="button" className="navbar-toggle" onClick={this.toggleMenu} style={{position: 'absolute', right: 0, top: 0}}>
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            </span>} fluid={true}  style={ {margin: 0} }>
+            <ul className="nav navbar-top-links navbar-right">
+              <Nav style={ {margin: 0} }>
                 
+                <NavDropdown title=<i className="fa fa-envelope fa-fw"></i> >
+                  <MenuItem eventKey="1">
+                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+
+                  </MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="2">
+                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+                  </MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="3">
+                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div> 
+                  </MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="4">
+                    <strong>Read All Messages</strong> <i className="fa fa-angle-right"></i>
+                  </MenuItem>
+                </NavDropdown>
+
+                <NavDropdown title=<i className="fa fa-tasks fa-fw"></i> >
+                  <MenuItem eventKey="1" style={ {width: 300} }>
+                    <div> 
+                      <p> <strong>Task 1</strong> <span className="pull-right text-muted">40% Complete</span> </p> 
+                      <div>
+                        <ProgressBar active bsStyle="success" now={40} /> 
+                      </div> 
+                    </div>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="2">
+                    <div> 
+                      <p> <strong>Task 2</strong> <span className="pull-right text-muted">20% Complete</span> </p> 
+                      <div>
+                        <ProgressBar active bsStyle="info" now={20} />
+                      </div> 
+                    </div> 
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="3">
+                    <div> 
+                      <p> <strong>Task 3</strong> <span className="pull-right text-muted">60% Complete</span> </p> 
+                      <div>
+                        <ProgressBar active bsStyle="warning" now={60} />
+                      </div> 
+                    </div> 
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="4">
+                    <div> 
+                      <p> <strong>Task 4</strong> <span className="pull-right text-muted">80% Complete</span> </p> 
+                      <div>
+                        <ProgressBar active bsStyle="danger" now={80} />
+                      </div> 
+                    </div>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="5">
+                      <strong>See All Tasks</strong> <i className="fa fa-angle-right"></i>
+                  </MenuItem>
+                </NavDropdown>
+
+                <NavDropdown title=<i className="fa fa-bell fa-fw"></i> >
+                  <MenuItem eventKey="1" style={ {width: 300} }>
+                    <div> <i className="fa fa-comment fa-fw"></i> New Comment <span className="pull-right text-muted small">4 minutes ago</span> </div>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="2">
+                    <div> <i className="fa fa-twitter fa-fw"></i> 3 New Followers <span className="pull-right text-muted small">12 minutes ago</span> </div>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="3">
+                    <div> <i className="fa fa-envelope fa-fw"></i> Message Sent <span className="pull-right text-muted small">4 minutes ago</span> </div>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="4">
+                    <div> <i className="fa fa-tasks fa-fw"></i> New Task <span className="pull-right text-muted small">4 minutes ago</span> </div>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="5">
+                    <div> <i className="fa fa-upload fa-fw"></i> Server Rebooted <span className="pull-right text-muted small">4 minutes ago</span> </div>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="6">
+                    <strong>See All Alerts</strong> <i className="fa fa-angle-right"></i>
+                  </MenuItem>
+                </NavDropdown>
+
+                <NavDropdown title=<i className="fa fa-user fa-fw"></i> >
+                  <MenuItem eventKey="1">
+                    <i className="fa fa-user fa-fw"></i> User Profile
+                  </MenuItem> 
+                  <MenuItem eventKey="2">
+                    <i className="fa fa-gear fa-fw"></i> Settings
+                  </MenuItem> 
+                  <MenuItem eventKey="3">
+                    <a href="http://www.strapui.com/" onClick={ () => { window.location='http://www.strapui.com/'; } }>
+                      <i className="fa fa-eye fa-fw"></i> Premium React Themes
+                    </a>
+                  </MenuItem> 
+                  <MenuItem divider />
+                  <MenuItem eventKey="4">
+                    <Link to="login">
+                      <i className="fa fa-sign-out fa-fw"></i> Logout
+                    </Link>
+                  </MenuItem>
+                </NavDropdown>
+
+              </Nav>
+            </ul> 
             <div className="navbar-default sidebar" style={ { 'marginLeft': '-20px' } } role="navigation">
               <div className="sidebar-nav navbar-collapse">
                 
